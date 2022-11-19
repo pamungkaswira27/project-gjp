f7bf504f5b4b74489434a84f25bb584247910896
Asuranimation <118593738+Asuranimation@users.noreply.github.com>
2022-11-19 22:22:24 +0700

    EnemyMove

---
+++ Assets/Script/Enemyahaha.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyahaha : MonoBehaviour
+{
+    [Header("MOVEMENT")]
+    public float moveSpeed;
+    public bool isFacingRight;
+    
+    //cheker
+    public Transform groundChecker;
+    public float groundChekerRadius;
+    public LayerMask whatIsGround;
+
+    //public Transform hudBar;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right *moveSpeed*Time.deltaTime);
+        if (!ThereIsGround())
+        { 
+            if (isFacingRight)
+            {
+               // hudBar.localEulerAngles = Vector2.zero;
+                transform.eulerAngles = Vector2.up * 180;
+                isFacingRight = false;
+            }
+            else
+            {
+               // hudBar.localEulerAngles = Vector2.up * 180; ;
+                transform.eulerAngles = Vector2.zero;
+                isFacingRight = true; 
+            }
+        }
+    }
+
+    bool ThereIsGround()
+    {
+        return Physics2D.OverlapCircle(groundChecker.position, groundChekerRadius, whatIsGround
+        );
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundChecker.position,groundChekerRadius);
+    }
+}

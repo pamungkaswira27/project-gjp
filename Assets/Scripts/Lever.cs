98174d7ee40a01bc99ce2c4c2a716a0363628daf
pamungkaswira27 <pamungkaswira27@gmail.com>
2022-11-20 20:29:18 +0700

    Devlog #3
    - Finsihing

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lever : MonoBehaviour
 {
     [SerializeField] MovingPlatform movingPlatform;
+    [SerializeField] Sprite[] switchImage;
     [SerializeField] Transform detector;
     [SerializeField] float detectorRadius;
     [SerializeField] LayerMask playerLayer;
+
+    SpriteRenderer spriteRenderer;
+    int index = 1;
+
+    void Awake()
+    {
+         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
@@ -14,6 +24,13 @@ public class Lever : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E))
             {
                 movingPlatform.TurnOnPlatform();
+                spriteRenderer.sprite = switchImage[index];
+
+                index++;
+                if (index >= switchImage.Length)
+                {
+                    index = 0;
+                }
             }
         }
     }
